9f3598393f6883559139ab07d32985a560624b8a
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-04-22 16:25:19 +0545

    update sanity.js

--- sanity.js
+++ sanity.js
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "pv8y60vp"
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION // "2022-11-16"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'pv8y60vp'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-11-16'
 
 export const config = {
   projectId,
